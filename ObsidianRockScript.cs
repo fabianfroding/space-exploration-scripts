fe049bec8c469a7387a288a9ad2cbc2089e51903
fabianfroding <fabian.froding@gmail.com>
2020-07-21 15:24:50 +0200

    impl planet behaviours

---
+++ ObsidianRockScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObsidianRockScript : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        rb.AddForce(new Vector3(Random.value, Random.value, Random.value) * 900f);
+
+        Invoke("DestroySelf", 15f);
+    }
+
+    private void DestroySelf()
+    {
+        Destroy(this.gameObject);
+    }
+}
